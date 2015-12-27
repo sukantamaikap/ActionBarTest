64990058ab77d6c63caff857b0b0d5ace0eef292
sukantamaikap <sukanta.maikap@gmail.com>
2015-12-27 08:40:57 +0530

    Nav Bar more

---
+++ app/src/main/java/bitsandpizzas/test/com/bitzpizzaz/MainActivity.java
@@ -0,0 +1,57 @@
+package bitsandpizzas.test.com.bitzpizzaz;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ShareActionProvider;
+
+public class MainActivity extends Activity {
+
+    private ShareActionProvider _shareActionProvider;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void share (Menu menu) {
+        MenuItem item = menu.findItem(R.id.action_share);
+        this._shareActionProvider = (ShareActionProvider) item.getActionProvider();
+        setIntent("Sample Text ....");
+    }
+
+    private void setIntent (String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        this._shareActionProvider.setShareIntent(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        share(menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent orderIntent = new Intent(this, OrderActivity.class);
+                startActivity(orderIntent);
+                return true;
+
+            case R.id.action_setting:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/bitsandpizzas/test/com/bitzpizzaz/OrderActivity.java
@@ -0,0 +1,17 @@
+package bitsandpizzas.test.com.bitzpizzaz;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends Activity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
