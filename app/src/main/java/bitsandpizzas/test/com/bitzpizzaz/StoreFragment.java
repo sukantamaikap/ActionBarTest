66e8fa72794063ac3969ccdf1bf7078e7831d35b
sukantamaikap <sukanta.maikap@gmail.com>
2016-01-11 19:51:21 +0530

    Adding the side bar part 1

---
+++ app/src/main/java/bitsandpizzas/test/com/bitzpizzaz/StoreFragment.java
@@ -0,0 +1,32 @@
+package bitsandpizzas.test.com.bitzpizzaz;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StoreFragment extends ListFragment {
+
+    public StoreFragment () {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.stores));
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
